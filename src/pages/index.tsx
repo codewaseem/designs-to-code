3a07f7174efe305ca6093fb5539674419bb55249
Waseem Ahmed <codewaseem@gmail.com>
2020-04-19 18:25:53 +0530

    bg setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,42 @@
 import React from "react";
 import { injectIntl, InjectedIntlProps } from "gatsby-plugin-intl";
+import { css } from "@emotion/core";
 import { Layout } from "~/components/Layout";
 import { SEO } from "~/components/SEO";
-import { css } from "@emotion/core";
-import { Theme } from "../styled";
+import { ReactComponent as BgPattern } from "~/assets/images/bg-pattern.svg";
 
 const Index: React.FC<InjectedIntlProps> = () => {
   return (
     <Layout>
       <SEO title="Hello" />
       <div
-        css={(theme: Theme) => css`
-          padding: 150px;
-          background: ${theme.color.secondary.accent};
+        css={css`
+          background: #28223f;
+          min-height: 100vh;
+          min-width: 100vw;
         `}
       >
-        <h1>Hello, World</h1>
+        <div
+          css={css`
+            display: flex;
+            height: 100vh;
+            justify-content: center;
+            align-items: center;
+          `}
+        >
+          <div
+            css={css`
+              width: 435px;
+              height: 635px;
+              background: #231e39;
+              border-radius: 5px;
+              position: relative;
+              box-shadow: 0 30px 10px -20px #1d1832;
+            `}
+          >
+            af
+          </div>
+        </div>
       </div>
     </Layout>
   );
